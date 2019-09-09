01e62de32ad31f3341cfaaa3e87b24284944d34b
John Cummins <john.cummins742@gmail.com>
2019-09-09 00:29:55 -0400

    completing lab 1

---
+++ Lab1_ASP.NET_jccummin_SN0883640/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab1_ASP.NET_jccummin_SN0883640.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
